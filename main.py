cf6bfd08ff160be9ad675e7345737d53306c64f9
Aadya Jha <aadya@Aadyas-MacBook-Pro.local>
2026-09-17 00:41:30 +0100

    Stage 0: configure Supabase authentication

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from fastapi.responses import JSONResponse
 from database import init_db, get_connection
 from pydantic import BaseModel
 '''import sqlite3'''
+import os
+from dotenv import load_dotenv
+from supabase import create_client
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = FastAPI()
 init_db()
